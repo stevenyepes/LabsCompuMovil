cdf85cb5a096660924a517d13bf3ea886df5992f
stevenyepes <thedemonsspeed@gmail.com>
2016-09-06 17:16:40 -0500

    Lab 2

---
+++ Lab1UI/lab2activities/src/main/java/co/edu/udea/compumovil/gr4/lab2activities/MainActivity.java
@@ -0,0 +1,13 @@
+package co.edu.udea.compumovil.gr4.lab2activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Lab1UI/lab2activities/src/main/java/co/edu/udea/compumovil/gr4/lab2activities/co/edu/udea/compumovil/gr4/lab2activities/entities/Place.java
@@ -0,0 +1,7 @@
+package co.edu.udea.compumovil.gr4.lab2activities.co.edu.udea.compumovil.gr4.lab2activities.entities;
+
+/**
+ * Created by Steven on 06/09/2016.
+ */
+public class Place {
+}
