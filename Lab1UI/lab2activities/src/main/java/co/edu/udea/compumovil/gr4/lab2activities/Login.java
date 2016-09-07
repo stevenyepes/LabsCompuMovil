9e59ac7fc90fd494143f5236d6b52262e55b3378
stevenyepes <thedemonsspeed@gmail.com>
2016-09-06 23:01:30 -0500

    Register done

--- Lab1UI/lab2activities/src/main/java/co/edu/udea/compumovil/gr4/lab2activities/Login.java
+++ Lab1UI/lab2activities/src/main/java/co/edu/udea/compumovil/gr4/lab2activities/Login.java
@@ -1,9 +1,13 @@
 package co.edu.udea.compumovil.gr4.lab2activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
+
+    private static final String TAG = ".intent.action.MAIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +15,31 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
     }
+
+    public void onLoginClick(View view) {
+
+        // Check if the user and password field are valid
+
+
+        // Check if the user exist
+
+        // Check if use and password are correct
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSignUpClick(View view) {
+
+        // Check if the user and password field are valid
+
+
+        // Check if the user exist
+
+        // Check if use and password are correct
+
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
--- Lab1UI/lab2activities/src/main/java/co/edu/udea/compumovil/gr4/lab2activities/RegisterActivity.java
+++ Lab1UI/lab2activities/src/main/java/co/edu/udea/compumovil/gr4/lab2activities/RegisterActivity.java
@@ -2,12 +2,58 @@ package co.edu.udea.compumovil.gr4.lab2activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import co.edu.udea.compumovil.gr4.lab2activities.entities.User;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    private Button btnRegister;
+    private Button btnLinkToLogin;
+    private EditText inputFullName;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private EditText inputAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        inputFullName = (EditText) findViewById(R.id.name);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        inputAge = (EditText) findViewById(R.id.age);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnLinkToLogin = (Button) findViewById(R.id.btnLinkToLoginScreen);
+
     }
+
+    public void onButtonRegisterClick(View view) {
+
+        String name = inputFullName.getText().toString();
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String age_s = inputAge.getText().toString();
+
+        if (!name.isEmpty() && !email.isEmpty() && !password.isEmpty() && !age_s.isEmpty()) {
+            int age = Integer.parseInt(age_s);
+            User user = new User(name, email, password, age);
+            user.save();
+            Toast.makeText(getApplicationContext(),
+                    "Congratulations! your account was created", Toast.LENGTH_SHORT)
+            .show();
+
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter your details!", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+
 }
