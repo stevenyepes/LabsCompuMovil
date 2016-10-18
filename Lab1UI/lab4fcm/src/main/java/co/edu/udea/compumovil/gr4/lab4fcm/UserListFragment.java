b5b51ba13395e7902ea101a5155bdaf044e1f867
Steven <jsteven.yepes@gmail.com>
2016-10-18 09:57:46 -0500

    fix merge

---
+++ Lab1UI/lab4fcm/src/main/java/co/edu/udea/compumovil/gr4/lab4fcm/UserListFragment.java
@@ -0,0 +1,31 @@
+package co.edu.udea.compumovil.gr4.lab4fcm;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UserListFragment extends Fragment {
+
+
+    public UserListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_user_list, container, false);
+
+        return view;
+    }
+
+}
